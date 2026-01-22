52195e7642fd432ef3cd73c5a983b65a0701f4b6
jnaneshjn2005-pixel <jnaneshjn2005@gmail.com>
2026-01-22 12:35:40 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("notes-cache").then(cache =>
-      cache.addAll([
+    caches.open("notes-cache").then(cache => {
+      return cache.addAll([
         "index.html",
         "dashboard.html",
         "admin.html",
         "style.css",
         "script.js"
-      ])
-    )
+      ]);
+    })
   );
 });
